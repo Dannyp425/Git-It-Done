a8e474d7cd55be6367f4c502a3cafb8790831de4
Daniel Pereira <54162368+Dannyp425@users.noreply.github.com>
2021-03-04 18:08:19 -0700

    linked pages and error handling

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,19 +1,40 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function() {
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+
+    if (repoName) {
+        //display repo name on the page
+        repoNameEl.textContent = repoName;
+
+        getRepoIssues(repoName);
+    } else {
+        //if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
+};
 
 var getRepoIssues = function (repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+    // make a get request to url
     fetch(apiUrl).then(function (response) {
+        //if request was successful
         if (response.ok) {
             response.json().then(function (data) {
                 displayIssues(data);
 
+            //check if API has paginated issues
             if (response.headers.get("Link")) {
                 displayWarning(repo);
             }
             });
         } else {
-            alert("There was a problem with your request!");
+            //if not successful, redirect to homepage 
+            document.location.replace("./index.html");
         }
     });
 };
@@ -27,7 +48,7 @@ var displayIssues = function (issues) {
 
     for (var i = 0; i < issues.length; i++) {
         var issueEl = document.createElement("a");
-        issueEl.classList = "list-tem flex-row justify-space-between align-center";
+        issueEl.classList = "list-item flex-row justify-space-between align-center";
         issueEl.setAttribute("href", issues[i].html_url);
         issueEl.setAttribute("target", "_blank");
         var titleEl = document.createElement("span");
@@ -57,4 +78,5 @@ var displayWarning = function(repo) {
 
     limitWarningEl.appendChild(linkEl);
 };
-getRepoIssues("facebook/react");
+
+getRepoName();
